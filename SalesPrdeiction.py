b7bf6645365311d239b9b9397f51dce3b5c1b18b
vsei8678 <31417851+vsei8678@users.noreply.github.com>
2017-08-28 15:04:05 -0400

    Add files via upload

---
+++ SalesPrdeiction.py
@@ -0,0 +1,13 @@
+# CTI-110
+# M2T1-Sales Prdediction
+# Vincent Sei
+# August 28, 2017
+
+# Get the Projectes total Sales
+total_sales = float(input('Enter the projected sales:'))
+# Calculate the profit as 23 percent of total sales
+profit = total_sales * 0.23
+# Display the profit
+print('The Profit is $',format(profit, ",.2f"))
+
+                               
