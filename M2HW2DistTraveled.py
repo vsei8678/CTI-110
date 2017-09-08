fad6f0ecf188e9eadf0e0dab29db83276c829f36
vsei8678 <31417851+vsei8678@users.noreply.github.com>
2017-09-08 12:08:12 -0400

    Add files via upload

---
+++ M2HW2DistTraveled.py
@@ -0,0 +1,24 @@
+# CTI-110
+# M2HW1(Module 2, Homework 1)- Distance Traveled
+# Vincent Sei
+# September 2, 2017
+
+# Program calculates distance traveled by a car using Dist = Speed * Time
+
+speed = float(input("what is the speed of the car:"))
+time1 = 6
+time2 = 10
+time3 = 2
+
+distanceAfter6 = speed*time1
+distanceAfter10 = speed*time2
+distanceAfter20 = speed*time3
+print("distanceafter6:miles"+format(distanceafter6,",.2f"),\
+      "distanceafter10:miles"+format(distanceafter10,",.2f"),\
+    "distanceafter20:miles"+format(distanceafter20,",.2f")
+
+
+
+      
+      
+    
