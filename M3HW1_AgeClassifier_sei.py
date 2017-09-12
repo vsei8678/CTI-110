9443a18b79c741c97688bd530020c557e8047338
vsei8678 <31417851+vsei8678@users.noreply.github.com>
2017-09-12 16:32:13 -0400

    Add files via upload

---
+++ M3HW1_AgeClassifier_sei.py
@@ -0,0 +1,20 @@
+# CTI-110
+# M3HW1-Age Classifier
+# Vincent Sei
+# September 9, 2017
+
+# get user entries
+userAge = int(input("Enter your Age:"))
+
+if userAge <= 1:
+    print("\nYou are an infant")
+elif userAge < 13:
+    print("\nYou are a child")
+elif userAge < 20:
+    print("\nYou are a teenager")
+elif userAge >= 20:
+    print("\nYou are an adult")
+
+        
+
+    
