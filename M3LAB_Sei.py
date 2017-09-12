fa59cce42d8fca4afe7941d6e4e1989eb1314a89
vsei8678 <31417851+vsei8678@users.noreply.github.com>
2017-09-12 16:33:02 -0400

    Add files via upload

---
+++ M3LAB_Sei.py
@@ -0,0 +1,40 @@
+# M3TLAB: Debugging
+# Sei
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+
+    # system uses 10-point grading scale
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60
+    
+
+    
+    score = float(input('Enter grade: '))
+
+    if score >= A_score:
+         print('Your grade is: A.')
+    else:
+        if score >= B_score:
+            print('Your grade is: B.')
+        else:
+             if score >= C_score:
+                    print('Your grade is C.')
+             else:
+                 if score >= D_score:
+                      print('Your grade is D.')       
+                 else:
+                     print('Your grade is: F.')
+
+                 # TO DO: finish this
+
+
+
+
+
+
+
+# program start
+main()
